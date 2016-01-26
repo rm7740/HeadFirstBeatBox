aa2fddc4a7af6126a56812478f728d3451b44bf0
rm7740 <adavid8a@hotmail.com>
2016-01-26 17:38:52 -0500

    Refactor play() to change instrument

--- src/MiniMiniMusicApp.java
+++ src/MiniMiniMusicApp.java
@@ -11,6 +11,10 @@ public class MiniMiniMusicApp {
             Sequence sequence = new Sequence(Sequence.PPQ, 4);
             Track track = sequence.createTrack();
 
+            ShortMessage first = new ShortMessage();
+            first.setMessage(192, 1, 102, 0);
+            MidiEvent changeInstrument = new MidiEvent(first, 0);
+            track.add(changeInstrument);
             ShortMessage a = new ShortMessage();
             a.setMessage(144, 1, 44, 100);
             MidiEvent noteOn = new MidiEvent(a, 1);
