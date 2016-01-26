5fe637c78e0b7cd91d25b6792088238c40da71a0
rm7740 <adavid8a@hotmail.com>
2016-01-26 17:21:01 -0500

    Add the call to play method in Main class

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@
  * Created by Dell on 26-Jan-16.
  */
 public class Main {
+    public static void main(String[] args) {
+        MiniMiniMusicApp mini = new MiniMiniMusicApp();
+        mini.play();
+    }
 }
