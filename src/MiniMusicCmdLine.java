be566beffd91f9274ad20f8f5aa9b9b3190c878f
rm7740 <adavid8a@hotmail.com>
2016-01-26 17:52:39 -0500

    Define play() in MiniMusicCmdLine

--- src/MiniMusicCmdLine.java
+++ src/MiniMusicCmdLine.java
@@ -1,5 +1,38 @@
 /**
  * Created by Dell on 26-Jan-16.
  */
+import javax.sound.midi.*;
+
 public class MiniMusicCmdLine {
+
+    public void play(int instrument, int note) {
+        try{
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+            Sequence sequence = new Sequence(Sequence.PPQ, 4);
+            Track track = sequence.createTrack();
+
+            MidiEvent event = null;
+
+            ShortMessage first = new ShortMessage();
+            first.setMessage(192, 1, instrument, 0);
+            MidiEvent changeInstrument = new MidiEvent(first, 1);
+            track.add(changeInstrument);
+
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, note, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            track.add(noteOn);
+            ShortMessage b = new ShortMessage();
+            a.setMessage(128, 1, note, 100);
+            MidiEvent noteOff = new MidiEvent(b, 16);
+            track.add(noteOff);
+
+            player.setSequence(sequence);
+            player.start();
+            
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
 }
